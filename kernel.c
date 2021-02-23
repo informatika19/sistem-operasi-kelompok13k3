477069623cdb598e0acf0214b3050d986d94a5f1
Iky <rifkyvirtualacc@gmail.com>
2021-02-23 09:25:54 +0700

    finish printString

--- kernel.c
+++ kernel.c
@@ -1,3 +1,8 @@
+void handleInterrupt21 (int AX, int BX, int CX, int DX);
+void printString(char *string);
+void readString(char *string);
+void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+
 int main () {
   putInMemory(0xB000, 0x8000, 'H');
   putInMemory(0xB000, 0x8001, 0xD);
@@ -6,8 +11,48 @@ int main () {
   putInMemory(0xB000, 0x8004, 'i');
   putInMemory(0xB000, 0x8005, 0xD);
 
+// printString test
+  printString("print string\ntest");
+
   while (1);
 }
 
-void handleInterrupt21 (int AX, int BX, int CX, int DX) {}
+void handleInterrupt21 (int AX, int BX, int CX, int DX){
+  switch (AX) {
+    case 0x0:
+      printString(BX);
+      break;
+    case 0x1:
+      readString(BX);
+      break;
+    default:
+      printString("Invalid interrupt");
+  }
+}
 
+
+void printString(char *string){
+	int i = 0;
+	int nl = 0;
+	while (string[i] != '\0') {
+		// jika ada newline agar print dilanjutkan dari kiri layar
+		if (string[i] == '\n') {
+			nl = 1;
+			break;
+		}
+		interrupt(0x10, 0xE00 + string[i], 0, 0, 0);
+		i = i+1;	
+	}
+	interrupt(0x10, 0xE00 + '\n', 0, 0, 0);
+	interrupt(0x10, 0xE00 + '\r', 0, 0, 0);
+	if(nl) {
+		printString(&string[i+1]);
+	}
+}
+
+void readString(char *string) {
+
+}
+void clear(char *buffer, int length) {
+
+}
