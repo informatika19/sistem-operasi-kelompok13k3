3cb0c8e7b54f31cffa3ef8a4d4534033aa58af59
mgstabrani <mgstabrani19@gmail.com>
2021-02-24 14:31:51 +0700

    Add boot logo ascii art

--- kernel.c
+++ kernel.c
@@ -2,19 +2,28 @@ void handleInterrupt21 (int AX, int BX, int CX, int DX);
 void printString(char *string);
 void readString(char *string);
 void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+void printLogo();
+void printInMemory(char *string, int color, int x, int y);
 
 int main () {
-  putInMemory(0xB000, 0x8000, 'H');
-  putInMemory(0xB000, 0x8001, 0xD);
-  putInMemory(0xB000, 0x8002, 'a');
-  putInMemory(0xB000, 0x8003, 0xD);
-  putInMemory(0xB000, 0x8004, 'i');
-  putInMemory(0xB000, 0x8005, 0xD);
 
-// printString test
-  printString("print string\ntest");
+	//variabel input saat readString
+	char input[255];
 
-  while (1);
+	//Menampilkan logo saat booting
+	printLogo();
+
+	//Memulai
+	printString("\nStarting my OS...");
+
+	//Membaca string
+	printString("What is your name: ");
+	readString(input);
+
+	//Selesai
+	printString("\nNice to meet you :)");
+
+	while (1);
 }
 
 void handleInterrupt21 (int AX, int BX, int CX, int DX){
@@ -30,7 +39,7 @@ void handleInterrupt21 (int AX, int BX, int CX, int DX){
   }
 }
 
-
+//Prosedur menampilkan string
 void printString(char *string){
 	int i = 0;
 	int nl = 0;
@@ -50,6 +59,7 @@ void printString(char *string){
 	}
 }
 
+//Prosedur baca string
 void readString(char *string) {
 	int i = 0;
 	char c = 0;
@@ -84,10 +94,48 @@ void readString(char *string) {
 	interrupt(0x10, 0xE00 + '\n', 0, 0, 0);
 	interrupt(0x10, 0xE00 + '\r', 0, 0, 0);
 }
+
+//Prosedur menghapus buffer dengan mengisi seluruh elemen buffer menjadi 0x00
 void clear(char *buffer, int length) {
 	//Mengisi seluruh elemen buffer dengan 0x00
 	int i;
 	for(i = 0; i < length; i++){
 		buffer[i] = 0x00;
 	}
+}
+
+//Menampilkan string ke dalam memory
+void printInMemory(char *string, int color, int x, int y)
+{
+	if (x >= 0 && x <= 80 && y >= 0 && y <= 25)
+	{
+		int i = 0;
+		while (string[i] != '\0' && string[i] != '\n')
+		{
+			int offset = 0x8000 + ((80 * y) + x) * 2;
+			putInMemory(0xB000, offset, string[i]);
+			putInMemory(0xB000, offset + 1, color);
+			i++;
+			x++;
+		}
+	}
+}
+
+//Menampilkan logo ascii saat booting
+void printLogo()
+{
+	printInMemory("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *", 0xA, 0, 0);
+	printInMemory("* ##           ##                                 ##     ##  ###  ##     ###  *", 0xA, 0, 1);
+	printInMemory("*  #            #                                  #      #  #  #  #     #  # *", 0xA, 0, 2);
+	printInMemory("*  #            #                                  #      #     #  #        # *", 0xA, 0, 3);
+	printInMemory("*  # ###  ###   #   ####  ########  #####   ####   # ###  #   ##   # ###  ##  *", 0xA, 0, 4);
+	printInMemory("*  # ##  #   #  #  ##  ##  #  #  #   #   # ##  ##  # ##   #     #  # ##     # *", 0xA, 0, 5);
+	printInMemory("*  ##    #####  #  #    #  #  #  #   #   # #    #  ##     #     #  ##       # *", 0xA, 0, 6);
+	printInMemory("*  # #   #      #  ##  ##  #  #  #   #   # ##  ##  # #    #  #  #  # #   #  # *", 0xA, 0, 7);
+	printInMemory("*  # #   #      #  ##  ##  #  #  #   #   # ##  ##  # #    #  #  #  # #   #  # *", 0xA, 0, 8);
+	printInMemory("* ### ##  #### ###  ####  #########  ####   ####  ### ## ### ###  ### ## ###  *", 0xA, 0, 9);
+	printInMemory("*                                    #                                        *", 0xA, 0, 10);
+	printInMemory("*                                   ###                                       *", 0xA, 0, 11);
+	printInMemory("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *", 0xA, 0, 12);
+
 }
