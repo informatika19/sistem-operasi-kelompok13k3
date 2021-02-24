f4703f701fb4981b5d17a2489017fcf00a65a3a5
ghallyy <ghallyarrahman12@gmail.com>
2021-02-24 09:32:47 +0700

    readString

--- kernel.c
+++ kernel.c
@@ -51,7 +51,38 @@ void printString(char *string){
 }
 
 void readString(char *string) {
-
+	int i = 0;
+	char c = 0;
+	while (c != '\r')
+	{
+		c = interrupt(0x16, 0, 0, 0, 0);
+		if (c == '\r')
+		{
+			string[i] = '\0';
+		}
+		else
+		{
+			if (c == '\b')
+			{
+				interrupt(0x10, 0xE00 + '\b', 0, 0, 0);
+				interrupt(0x10, 0xE00 + '\0', 0, 0, 0);
+				interrupt(0x10, 0xE00 + '\b', 0, 0, 0);
+				if (i > 0)
+				{
+					string[i] = '\0';
+					i = i - 1;
+				}
+			}
+			else
+			{
+				string[i] = c;
+				i = i + 1;
+				interrupt(0x10, 0xE00 + c, 0, 0, 0);
+			}
+		}
+	}
+	interrupt(0x10, 0xE00 + '\n', 0, 0, 0);
+	interrupt(0x10, 0xE00 + '\r', 0, 0, 0);
 }
 void clear(char *buffer, int length) {
 	//Mengisi seluruh elemen buffer dengan 0x00
