24b137df5baf1acc2f225fb1aee8321ae63477f0
mgstabrani <mgstabrani19@gmail.com>
2021-02-24 14:30:57 +0700

    Fix clear procedure

--- kernel.c
+++ kernel.c
@@ -86,7 +86,8 @@ void readString(char *string) {
 }
 void clear(char *buffer, int length) {
 	//Mengisi seluruh elemen buffer dengan 0x00
-	for(int i = 0; i < length; i++){
+	int i;
+	for(i = 0; i < length; i++){
 		buffer[i] = 0x00;
 	}
 }
