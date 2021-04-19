c819a67cf1432e7f85c5ee84e609ebc417290cb9
mgstabrani <mgstabrani19@gmail.com>
2021-04-19 10:02:17 +0700

    Edit compiling

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ dd if=map.img of=system.img bs=512 count=1 seek=256 conv=notrunc
 dd if=files.img of=system.img bs=512 count=2 seek=257 conv=notrunc
 dd if=sectors.img of=system.img bs=512 count=1 seek=259 conv=notrunc
 bcc -ansi -c -o kernel.o kernel.c
-bcc -ansi -c -o math.o math.c
-bcc -ansi -c -o text.o text.c
-bcc -ansi -c -o fileIO.o fileIO.c
+bcc -ansi -c -o math.o modul/math.c
+bcc -ansi -c -o text.o modul/text.c
+bcc -ansi -c -o fileIO.o modul/fileIO.c
 nasm -f as86 kernel.asm -o kernel_asm.o
 ld86 -o kernel -d kernel.o kernel_asm.o math.o text.o fileIO.o
 dd if=kernel of=system.img bs=512 conv=notrunc seek=1
@@ -29,7 +29,7 @@ ld86 -o cat -d cat.o lib_asm.o text.o fileIO.o math.o
 ./loadFile cat
 
 # compile mkdir
-bcc -ansi -c mkdir.c -o mkdir.o
+bcc -ansi -c command/mkdir.c -o mkdir.o
 ld86 -o mkdir -d mkdir.o lib_asm.o
 ./loadFile mkdir
 
@@ -38,8 +38,7 @@ ld86 -o mkdir -d mkdir.o lib_asm.o
 ./loadFile 13519123.txt
 
 # cleaning compiled files
-rm bochsout.txt bootloader cat cat.o fileIO.o kernel kernel.o kernel_asm.o
-rm lib_asm.o loadFile math.o mkdir mkdir.o shell shell.o text.o
+rm bochsout.txt bootloader cat cat.o fileIO.o kernel kernel.o kernel_asm.o lib_asm.o loadFile math.o mkdir mkdir.o shell shell.o text.o
 
 # run bochs
 echo c | bochs -f if2230.config
